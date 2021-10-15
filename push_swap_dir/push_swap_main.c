b033460a10c283b5779270e57c61f86fecba1756
Nickie Mustache <nmustach@am-k4.msk.21-school.ru>
2021-10-15 19:21:20 +0300

    delete DSstore

--- push_swap_dir/push_swap_main.c
+++ push_swap_dir/push_swap_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap_main.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmustach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nmustach <nmustach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 23:29:48 by nmustach          #+#    #+#             */
-/*   Updated: 2020/04/02 00:39:37 by nmustach         ###   ########.fr       */
+/*   Updated: 2021/10/15 19:20:51 by nmustach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ int	main(int argc, char **argv)
 {
 	t_node	*stack_a;
 	t_node	*stack_b;
+	size_t	m;
+	size_t	f;
 
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc >= 2)
 	{
-		if (!push_values_to_stack(argc, argv, &stack_a))
-			return (std_err(stack_a, stack_b));
+		if (!push_values_to_stack(argc, argv, &stack_a, &m))
+			return (std_err(stack_a, stack_b, &f));
 		if (!is_sorted(stack_a, stack_b) && stack_a->next)
 			sort(&stack_a, &stack_b);
-		free_both_stacks(stack_a, stack_b);
+		free_both_stacks(stack_a, stack_b, &f);
+		ft_printf("Malloc:%lu Free:%lu", m, f);
 	}
 	return (0);
 }
