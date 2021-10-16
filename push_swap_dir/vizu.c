e69e312a2bd5c5ac92c1a3ba10c68ec59234b157
Головко Артём Петрович <APeGolovko@sberbank.ru>
2021-10-16 12:28:55 +0300

    visu

--- push_swap_dir/vizu.c
+++ push_swap_dir/vizu.c
@@ -12,30 +12,26 @@
 
 #include "push_swap.h"
 
-void  printstack_vizu(t_node *stack_a, t_node *stack_b)
+void	printstack_vizu(t_node *stack_a, t_node *stack_b)
 {
-    ft_printf("\e[1;1H\e[2J");
-    ft_printf(ANSI_YELLOW"A       B"ANSI_RESET"\n__________\n");
-    while (stack_a || stack_b)
-    {
-        if (stack_a != NULL)
-        {
-            ft_printf("%d | ", stack_a->val);
-        }
-        else
+	ft_printf("\e[1;1H\e[2J");
+	ft_printf(ANSI_YELLOW"A       B"ANSI_RESET"\n__________\n");
+	while (stack_a || stack_b)
+	{
+		if (stack_a != NULL)
+			ft_printf("%d | ", stack_a->val);
+		else
 			ft_printf(ANSI_RED"x"ANSI_RESET"  | ");
-        if (stack_b != NULL)
-        {
-            ft_printf("%d", stack_b->val);
-        }
-        else
-            ft_printf(ANSI_RED"x"ANSI_RESET);
-        ft_printf("\n");
-        if (stack_a != NULL)
-        stack_a = stack_a->next;
-        if (stack_b != NULL)
-        stack_b = stack_b->next;
-    }
-     ft_printf("\n");
-    sleep(1);
+		if (stack_b != NULL)
+			ft_printf("%d", stack_b->val);
+		else
+			ft_printf(ANSI_RED"x"ANSI_RESET);
+		ft_printf("\n");
+		if (stack_a != NULL)
+			stack_a = stack_a->next;
+		if (stack_b != NULL)
+			stack_b = stack_b->next;
+	}
+	ft_printf("\n");
+	sleep(1);
 }
